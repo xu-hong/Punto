69decef73470edb54a89ab738fa873520ef683b1
xu-hong <xuhong713@gmail.com>
2021-09-20 15:30:47 +0800

    add to Data class

--- punto/sequence.py
+++ punto/sequence.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 # default velocity: mezzo forte
 VELOCITY = 64 
 
-def to_midi_value(n, up=0):
+def to_midi_value(n, up=0, sharp=0):
   #Octave 4, the default
   notes = {
     "C": 72,
@@ -19,9 +19,13 @@ def to_midi_value(n, up=0):
     "As": 82,
     "B": 83
   }
-  val = notes.get(n)
-  val = val + up * 12
+  val = notes.get(n, 72)
+  val = val + up * 12 + sharp
   return val
+
+
+class ParserError(Exception):
+    pass
 
 
 @dataclass
@@ -30,23 +34,67 @@ class Data:
     data2: int = VELOCITY
 
     @classmethod
-    def from_grammar(cls, g):
+    def from_grammar(cls, *g):
+        """
+        Accepts grammar like:
+        'E'
+        ('A', 2, 'up')
+        ('C', 1, 'sharp')
+        (('G', 2, 'sharp'), 2, 'down')
+        
+        """
+        g = g[0]
         if len(g) == 1:
-            data = to_midi_value(g)
-            return Data(data, cls.data2)
+            val = to_midi_value(g)
+            return Data(val)
+
+        elif len(g) == 3:
+            def _parse_directive(val, directive):
+                up = 0
+                if directive == "up":
+                    up = 1 * val
+                if directive == "down":
+                    up = -1 * val
+
+                sharp = 0
+                if directive == "sharp":
+                    sharp += val
+                if directive == "flat":
+                    sharp -= val
+                return up, sharp
+
+            up, sharp = _parse_directive(g[1], g[2])
+            e = g[0]
+            if len(e) == 3:
+                up0, sharp0 = _parse_directive(e[1], e[2])
+                up = up + up0
+                sharp = sharp + sharp0
+                e = e[0]
+            return Data(to_midi_value(e, up, sharp))
+        else:
+            raise ParserError(f"Malformed parsed message {g}")
 
 
 
 @dataclass
 class Message:
-    """A proxy for midi message"""
+    """A proxy for midi message
+
+    Accept grammar like:
+    'E'
+    ('E', 1)
+    ('A', 2, 'up')
+    (('G', 2, 'sharp'), 2, 'down')
+    ((('E', 1, 'flat'), 2, 'up'), 3)
+    
+    """
     time: float
     status: int
     data: Data
 
 
 if __name__ == '__main__':
-    d = Data.from_grammar("C")
-    m = Message(0.0, 0, d)
-    print(d)
-    print(m)
+    print(Data.from_grammar("C"))
+    print(Data.from_grammar(('A', 2, 'up')))
+    print(Data.from_grammar(('C', 1, 'sharp')))
+    print(Data.from_grammar((('G', 2, 'sharp'), 2, 'down')))
